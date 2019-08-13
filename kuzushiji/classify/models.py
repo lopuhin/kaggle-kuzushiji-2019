85f5369a89ff5cab6570505f9d0891aa9694fb5f
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-08-13 18:04:10 +0300

    Use layer2 and layer3 instead

--- kuzushiji/classify/models.py
+++ kuzushiji/classify/models.py
@@ -13,31 +13,31 @@ class Model(nn.Module):
     def __init__(self, n_classes: int):
         super().__init__()
         self.base = ResNetBase()
-        self.res_l3 = 3
-        self.res_l4 = 1
+        self.res_l1 = 3
+        self.res_l2 = 3
         self.head = Head(
-            in_features=(self.base.out_features_l3 * self.res_l3 ** 2 +
-                         self.base.out_features_l4 * self.res_l4 ** 2),
+            in_features=(self.base.out_features_l1 * self.res_l1 ** 2 +
+                         self.base.out_features_l2 * self.res_l2 ** 2),
             n_classes=n_classes)
 
     def forward(self, x):
         x, rois = x
         _, _, input_h, input_w = x.shape
-        x_l3, x_l4 = self.base(x)
+        x_l1, x_l2 = self.base(x)
         del x
-        x_l3 = roi_align(
-            x_l3, rois,
-            output_size=(self.res_l3, self.res_l3),
-            spatial_scale=x_l3.shape[3] / input_w,
+        x_l1 = roi_align(
+            x_l1, rois,
+            output_size=(self.res_l1, self.res_l1),
+            spatial_scale=x_l1.shape[3] / input_w,
         )
-        x_l4 = roi_align(
-            x_l4, rois,
-            output_size=(self.res_l4, self.res_l4),
-            spatial_scale=x_l4.shape[3] / input_w,
+        x_l2 = roi_align(
+            x_l2, rois,
+            output_size=(self.res_l2, self.res_l2),
+            spatial_scale=x_l2.shape[3] / input_w,
         )
         x = torch.cat(
-            [x_l3.flatten(start_dim=1),
-             x_l4.flatten(start_dim=1)],
+            [x_l1.flatten(start_dim=1),
+             x_l2.flatten(start_dim=1)],
             dim=1)
         x = self.head(x)
         return x
@@ -62,8 +62,8 @@ class ResNetBase(nn.Module):
     def __init__(self):
         super().__init__()
         self.base = models.resnet50(pretrained=True)
-        self.out_features_l3 = 1024
-        self.out_features_l4 = 2048
+        self.out_features_l1 = 512
+        self.out_features_l2 = 1024
 
     def forward(self, x):
         base = self.base
@@ -72,8 +72,7 @@ class ResNetBase(nn.Module):
         x = base.relu(x)
         x = base.maxpool(x)
         x = base.layer1(x)
-        x = base.layer2(x)
-        x_l3 = base.layer3(x)
+        x_l1 = base.layer2(x)
         del x
-        x_l4 = base.layer4(x_l3)
-        return x_l3, x_l4
+        x_l2 = base.layer3(x_l1)
+        return x_l1, x_l2
