5a1ab3a690c6d14e55366de52772bdef9c073506
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-08-25 20:07:05 +0300

    Add dropout, larger hidden dim

--- kuzushiji/lm/models.py
+++ kuzushiji/lm/models.py
@@ -9,16 +9,18 @@ class Model(nn.Module):
     def __init__(
             self, *,
             n_classes: int,
-            embedding_dim: int = 128,
-            hidden_dim: int = 128,
+            embedding_dim: int = 256,
+            hidden_dim: int = 256,
             ):
         super().__init__()
         self.embedding = nn.Embedding(n_classes, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.dropout = nn.Dropout(0.5)
         self.fc_out = nn.Linear(hidden_dim, n_classes)
 
     def forward(self, x):
         x = self.embedding(x)
         x, _ = self.lstm(x)
+        x = self.dropout(x)
         x = self.fc_out(x)
         return x.transpose(1, 2)
