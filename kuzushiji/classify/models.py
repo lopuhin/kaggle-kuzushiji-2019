ad6a6f2a7c1b8efafc28a2fcf7b28b041ba91f1c
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-09-09 00:44:04 +0300

    Add dropout to the head (to be tuned)

--- kuzushiji/classify/models.py
+++ kuzushiji/classify/models.py
@@ -52,11 +52,13 @@ class Head(nn.Module):
     def __init__(self, in_features: int, n_classes: int):
         super().__init__()
         hidden_dim = 1024
+        self.dropout = nn.Dropout(0.25)
         self.fc1 = nn.Linear(in_features, hidden_dim)
         self.bn = nn.BatchNorm1d(hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, n_classes)
 
     def forward(self, x):
+        x = self.dropout(x)
         x = F.relu(self.fc1(x))
         x = self.bn(x)
         x = self.fc2(x)
