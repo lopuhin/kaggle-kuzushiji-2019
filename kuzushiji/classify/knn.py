c144be32c12928f52c0c8211835d400b6a74db01
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-09-30 23:56:38 +0300

    Check metrics at different seg_fp thresholds

--- kuzushiji/classify/knn.py
+++ kuzushiji/classify/knn.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 from pathlib import Path
 
 import numpy as np
@@ -17,6 +18,8 @@ def main():
     arg('--device', default='cuda')
     arg('--limit', type=int, help='evaluate only on some pages')
     arg('--fp16', type=int, default=1)
+    arg('--check-thresholds', type=int)
+    arg('--threshold', type=float, default=0.0)
     args = parser.parse_args()
 
     clf_folder = Path(args.clf_folder)
@@ -47,14 +50,27 @@ def main():
 
     classes = get_encoded_classes()
     seg_fp_id = classes[SEG_FP]
+    thresholds = {args.threshold}
+    if args.check_thresholds:
+        thresholds.update(
+            np.linspace(0, 1, args.check_thresholds, endpoint=False))
+    thresholds = sorted(thresholds)
 
-    pred_ys = []
+    pred_ys_by_threshold = defaultdict(list)
     for i in tqdm.trange(test_features.shape[0]):
         feature = test_features[i].unsqueeze(1).to(device)
         sim = torch.mm(train_features, feature).squeeze()
-        # TODO: if sim is too low, output seg_fp, check best threshold
-        pred_ys.append(int(train_ys[sim.argmax()]))
-    pred_ys = np.array(pred_ys)
+        max_idx = sim.argmax()
+        max_sim = sim[max_idx]
+        cls = train_ys[max_idx]
+        for th in thresholds:
+            th_cls = cls
+            # TODO maybe also separate threshold for seg_fp?
+            if max_sim < th:
+                th_cls = seg_fp
+            pred_ys_by_threshold[th].append(th_cls)
+    pred_ys_by_threshold = {
+        th: np.array(pred_ys) for th, pred_ys in pred_ys_by_threshold.items()}
 
     # fn from missing detections missed by the segmentation model
     fn_segmentation = (
@@ -65,26 +81,27 @@ def main():
         pred=df_detailed['pred'].values,
         seg_fp=SEG_FP,
         fn_segmentation=fn_segmentation)
-    print('clf')
+    print('clf baseline')
     print_metrics(clf_metrics)
+    print()
 
-    knn_metrics = get_metrics(
-        true=df_detailed['true'].values,
-        pred=pred_ys,
-        seg_fp=seg_fp_id,
-        fn_segmentation=fn_segmentation)
-    print('knn')
-    print_metrics(knn_metrics)
-
+    for th in thresholds:
+        knn_metrics = get_metrics(
+            true=df_detailed['true'].values,
+            pred=pred_ys,
+            seg_fp=seg_fp_id,
+            fn_segmentation=fn_segmentation)
+        print(f'knn at threshold={th:.3f}')
+        print_metrics(knn_metrics)
+        print()
 
 
 def get_metrics(true, pred, seg_fp, fn_segmentation):
     accuracy = (true == pred).mean()
-    tp = ((true != 'seg_fp') & (pred == true)).sum()
-    fp = ((true == 'seg_fp') &
-          (pred != 'sef_fp')).sum()
-    fn = ((true != 'seg_fp') &
-          (pred == 'sef_fp')).sum() + fn_segmentation
+    tp = ((true != seg_fp) & (pred == true)).sum()
+    fp = ((true == seg_fp) & (pred != sef_fp)).sum()
+    fn = ((true != seg_fp) & (pred == sef_fp)).sum() + fn_segmentation
+    seg_fp_ratio = (pred == seg_fp).mean()
     if (tp + fp) == 0 or (tp + fn) == 0:
         f1 = 0
     else:
@@ -95,11 +112,12 @@ def get_metrics(true, pred, seg_fp, fn_segmentation):
         else:
             f1 = 0
     return {
-        'f1': float(f1),
+        'f1': f1,
+        'accuracy': accuracy,
         'tp': tp,
         'fp': fp,
         'fn': fn,
-        'accuracy': float(accuracy),
+        'seg_fp_ratio': seg_fp_ratio,
     }
 
 
