d6bd2bddd9f2e4035db4430327091348fd89cd65
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-10-14 22:38:02 +0300

    Finish run-all.sh

--- README.rst
+++ README.rst
@@ -13,24 +13,27 @@ Install::
     pip install -r requirements.txt
     python setup.py develop
 
-Install libjpeg-turbo as documented at https://github.com/ajkxyz/jpeg4py
+Install libjpeg-turbo for your OS as documented at https://github.com/ajkxyz/jpeg4py
 
-Install apex https://github.com/NVIDIA/apex (python-only is good enough)
+Install apex as documented at https://github.com/NVIDIA/apex
+(python-only is good enough)
 
 Run
 ---
 
-All steps required for submission are in ``./run-all.sh``.
+All steps required for submission are in heavily commented ``run-all.sh``
+and ``level2.sh``. Note that ``run-all.sh`` was never run in one step
+(it would take too much time), so may contain errors.
+Also all model parameters are in the ``models`` folder
+(they come from actual models).
 
-TODO port it as well, GPU memory requirements.
+One extra model was used to create pseudolabels which is not used in the final
+solution (resnext101_32x16d_wsl on fold0 with batch size 10,
+instead of resnext101_32x8d_wsl on the same fold),
+but it's contribution is extremely minor and
+quality is very similar to resnext101_32x8d_wsl.
 
-#. Create submission from classification model, using file created by the
-   previous command as the first argument::
+All run logs and configs (for classification models) are in ``_runs`` folder.
 
-    python -m kuzushiji.classify.main \
-        _runs/segment-fold0/test_predictions.csv \
-        --output-dir _runs/clf \
-        --resume _runs/clf/model_best.pth \
-        --submission
-
-.
+Overview
+--------
--- run-all.sh
+++ run-all.sh
@@ -21,13 +21,19 @@ do
         --submission
 done
 
-# python -c "import pandas as pd; pd.concat([pd.read_csv(f'_runs/segment-fold{i}/clf_gt.csv') for i in range(5)]).to_csv('_runs/segment_clf_gt.csv')"
+# Concatenate blend results
+python -c "
+import pandas as pd;
+pd.concat([pd.read_csv(f'_runs/segment-fold{i}/clf_gt.csv') for i in range(5)]).to_csv('_runs/segment_clf_gt.csv')
+"
 
+# Train classification models from scratch
 
 for fold in 0 1 2 3 4;
 do
     echo "fold ${fold}"
-    # Train classification model
+
+    # resnet152, requires a 16 GB GPU, most likely will fit  in 11 GB with --opt-level O1 (weak but still used)
     python -m kuzushiji.classify.main \
         _runs/segment_clf_gt.csv \
         --output-dir _runs/clf-fold${fold} --fold ${fold} \
@@ -41,7 +47,7 @@ do
         --print-model 0 \
         --n-tta 4 \
         --test-only > _runs/clf-fold${fold}/validation.txt
-    # Create submission (sementation: check different folds, blend)
+    # Create submission
     python -m kuzushiji.classify.main \
         _runs/segment-fold0/test_predictions.csv \
         --output-dir _runs/clf-fold${fold} --fold ${fold} \
@@ -50,6 +56,94 @@ do
         --print-model 0 \
         --n-tta 4 \
         --submission
+
+    # resnext101_32x8d_wsl: smallest of WSL models, still fits fine on 2080ti with fp16 and freezing
+    # Takes around 16h to train on 2080ti
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen --fold ${fold} --print-model 0 \
+        --max-targets 256 --benchmark 1 --frozen-start 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --lr 16e-3 --opt-level O1
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen/model_best.pth \
+        --n-tta 4 --test-only > _runs/clf-fold${fold}-wsl8d-mt256-frozen/validation.txt
+    python -m kuzushiji.classify.main \
+        _runs/segment-fold0/test_predictions.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen/model_best.pth \
+        --n-tta 4 --submission
+
 done
 
-# TODO blend
+# Create pseduolabels
+python -m kuzushiji.classify.pseudolabel \
+    _runs/clf-fold[0-4]-wsl8d-mt256-frozen/test_detailed.csv.gz \
+    _runs/clf-fold[0-4]/test_detailed.csv.gz \
+    _runs/pseudolabels.csv.gz
+
+# Train models on pseudolabels
+
+for fold in 0 1 2 3 4;
+do
+    echo "fold ${fold}"
+
+    # Fine-tuned pseudolabels model (~2h on 2080ti)
+    # Note that exact params for this model were not saved, and recovered from memory (logs were saved though)
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo-ft --fold ${fold} --print-model 0 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen/model_best.pth \
+        --max-targets 256 --benchmark 1 --frozen-start 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --lr 1.6e-3 --opt-level O1 \
+        --pseudolabels _runs/pseudolabels.csv.gz \
+        --repeat-train 3 \
+        --epochs 5
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo-ft --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo-ft/model_best.pth \
+        --n-tta 4 --test-only
+    python -m kuzushiji.classify.main \
+        _runs/segment-fold0/test_predictions.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo-ft/model_best.pth \
+        --n-tta 4 --submission
+
+    # Model on pseudolabels from scratch (abour 22h on 2080ti)
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo --fold ${fold} --print-model 0 \
+        --max-targets 256 --benchmark 1 --frozen-start 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --lr 16e-3 --opt-level O1 \
+        --pseudolabels _runs/pseudolabels.csv.gz \
+        --repeat-train 4
+    python -m kuzushiji.classify.main \
+        _runs/segment_clf_gt.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo/model_best.pth \
+        --n-tta 4 --test-only
+    python -m kuzushiji.classify.main \
+        _runs/segment-fold0/test_predictions.csv \
+        --output-dir _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo --fold ${fold} --print-model 0 \
+        --benchmark 1 \
+        --base resnext101_32x8d_wsl --workers 4 --batch-size 12 --opt-level O1 \
+        --resume _runs/clf-fold${fold}-wsl8d-mt256-frozen-pseudo/model_best.pth \
+        --n-tta 4 --submission
+
+done
+
+# create final submission with a second level model
+
+bash ./level2.sh
